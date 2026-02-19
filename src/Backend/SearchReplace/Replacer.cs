63a5c45b42fb7314e10e81b5d410abf17c650523
Lenni808090 <haselenni@gmail.com>
2026-02-19 02:43:56 +0100

    replacer ground structure

--- src/Backend/SearchReplace/Replacer.cs
+++ src/Backend/SearchReplace/Replacer.cs
@@ -1,6 +1,21 @@
 class Replacer {
-	List<char> charsUsedToReplace;
+
+	Buffer buffer;
+	List<char> charsUsedToReplace = new List<char>();
+
+	public Replacer(Buffer buffer) {
+		this.buffer = buffer;
+	}
+
 	public void replaceCurrentFindling() {
-		charsUsedToReplace = new();
+
+	}
+
+	public void replaceAllFindilngs() {
+
+	}
+
+	public void setCharsUsedToReplace(List<char> chars) {
+		charsUsedToReplace = chars;
 	}
 }
